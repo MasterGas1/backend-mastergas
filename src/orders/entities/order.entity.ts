f373c2d1b444d629137391739847d450af820eb8
ManuelBarba06 <113741996+ManuelBarba06@users.noreply.github.com>
2025-01-10 19:41:25 -0600

    Fix order problem

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,5 +1,5 @@
-import {Schema, Prop, SchemaFactory} from '@nestjs/mongoose';
-import mongoose, {Document} from 'mongoose';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
 import { Service } from 'src/service/entities/service.entity';
 import { User } from 'src/user/entities/user.entity';
 
@@ -51,7 +51,7 @@ export class Order extends Document {
 
   @Prop({
     enum: ['finished', 'proccess', 'on the way'],
-    default: 'proccess',
+    default: 'on the way',
   })
   state: string;
 
@@ -70,40 +70,6 @@ export class Order extends Document {
     default: Date.now,
   })
   createdAt: Date;
-    
-    // @Prop({
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: Service.name,
-    //     required: true
-    // })
-    // serviceId: Service
-
-    @Prop({
-        type: mongoose.Schema.Types.ObjectId,
-        ref: CompanyInstaller.name,
-        required: true
-    })
-    installerId: CompanyInstaller
-
-    @Prop({
-        type: mongoose.Schema.Types.ObjectId,
-        ref: User.name,
-        required: true
-    })
-    customerUserId: User
-
-    price: number
-
-    @Prop({
-        enum: ['finished', 'proccess', 'on te way'],
-        default: 'proccess'
-    })
-    state: string
-
-    @Prop({
-        default: Date.now
-    })
-    createdAt: Date
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order);
