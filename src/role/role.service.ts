2eedfba99dc87cb633d82df8f549c54835989f11
ManuelBarba06 <113741996+ManuelBarba06@users.noreply.github.com>
2025-01-14 21:17:33 -0600

    update role test

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -11,18 +15,20 @@ import { Role } from './entities/role.entity';
 export class RoleService {
   constructor(
     @InjectModel(Role.name)
-    private readonly roleModel: Model<Role> //Access to the model
-  ){}
+    private readonly roleModel: Model<Role>, //Access to the model
+  ) {}
 
+  async create(
+    createRoleDto: CreateRoleDto,
+  ): Promise<Role | BadRequestException> {
+    const roleExist = await this.roleModel.findOne({
+      name: createRoleDto.name,
+    });
 
-  async create(createRoleDto: CreateRoleDto): Promise<Role | BadRequestException> {
-      const roleExist = await this.roleModel.findOne({name: createRoleDto.name})
-      
-      if(roleExist) {
-        throw new BadRequestException('Role ya existe')
-      }
-      
-      return await this.roleModel.create(createRoleDto);
+    if (roleExist) {
+      throw new BadRequestException('Role ya existe');
+    }
+    return await this.roleModel.create(createRoleDto);
   }
 
   async findAll(): Promise<Role[]> {
@@ -35,39 +41,47 @@ export class RoleService {
     const role = await this.roleModel.findById(id);
 
     if (!role) {
-      throw new NotFoundException('Role no existe')
+      throw new NotFoundException('Role no existe');
     }
 
     return role;
   }
 
-  async update(id: string, updateRoleDto: UpdateRoleDto): Promise<Role | BadRequestException> {
+  async update(
+    id: string,
+    updateRoleDto: UpdateRoleDto,
+  ): Promise<Role | BadRequestException> {
     const roleExist = await this.roleModel.findById(id);
     if (!roleExist) {
-      throw new NotFoundException('Role no existe')
+      throw new NotFoundException('Role no existe');
     }
 
-    const roleRepeated = await this.roleModel.findOne({name: updateRoleDto.name})
-    if(roleRepeated) {
-      throw new BadRequestException('El nombre de rol ya existe')
+    const roleRepeated = await this.roleModel.findOne({
+      name: updateRoleDto.name,
+    });
+    if (roleRepeated) {
+      throw new BadRequestException('El nombre de rol ya existe');
     }
 
-    const roleUptaded = await this.roleModel.findByIdAndUpdate(id, updateRoleDto, {new: true});
+    const roleUptaded = await this.roleModel.findByIdAndUpdate(
+      id,
+      updateRoleDto,
+      { new: true },
+    );
 
-    return roleUptaded
+    return roleUptaded;
   }
 
   async remove(id: string) {
-    
     const category = await this.roleModel.findById(id);
 
     if (!category) {
-      throw new NotFoundException('Role no existe')
+      throw new NotFoundException('Role no existe');
     }
 
     await category.deleteOne();
 
-    return {}
+    return {};
   }
 
   async removeAllCollections() {
